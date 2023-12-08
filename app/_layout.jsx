c5f4c43764a0d029dcc9c823604c99e9cb74b965
vnfindjob <vnfindjob@gmail.com>
2023-12-08 09:21:30 +0700

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,22 @@
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+// import * as SplashScreen from "expo-splash-screen";
+
+// SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+	// Ensure any route can link back to `/`
+	initialRouteName: "home",
+};
+
+const Layout = () => {
+	
+	return (
+		<Stack initialRouteName="home">
+			<Stack.Screen name="home" />
+		</Stack>
+	);
+};
+
+export default Layout;
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			"expo-router/babel",
+			[
+				"module:react-native-dotenv",
+				{
+					moduleName: "react-native-dotenv",
+				},
+			],
+		],
+	};
 };
